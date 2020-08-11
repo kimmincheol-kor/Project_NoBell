f08397ed8facba8cef1a12f90bd3f0fcaf489984
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-08-11 13:21:26 +0900

    Create Project

---
+++ AndroidApp/Owner/app/src/main/java/com/nobell/owner/activity/OfficeActivity.java
@@ -0,0 +1,16 @@
+package com.nobell.owner.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nobell.owner.R;
+
+public class OfficeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office);
+    }
+}
---
+++ AndroidApp/Owner/app/src/main/java/com/nobell/owner/activity/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.nobell.owner.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
