1ae46033e3e3001e49a975e8037888df09cb715f
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2020-08-12 03:50:47 +0000

    Update : Implement Owner Registeration

--- WebServer/routes/owner.js
+++ WebServer/routes/owner.js
@@ -17,6 +17,51 @@ router.get('/', function(req, res, next) {
     res.send('success');
 });
 
+// Register
+router.post('/register', function(req, res, next) {
+
+  var reg_name = req.body.reg_name;
+  var reg_email = req.body.reg_email;
+  var reg_pwd = req.body.reg_pwd;
+  var reg_phone = req.body.reg_phone;
+
+  var check_data = [reg_email, reg_phone];
+  var reg_data = [reg_name, reg_email, reg_pwd, reg_phone];
+
+  console.log('[Register Request]');
+  console.log('-> Get Data = ', reg_data);
+  
+  pool.getConnection(function(err, connection){
+      
+      var sqlForSelectBoard = "select * FROM nobell.owner WHERE owner_email=? OR owner_phone=?";
+      
+      connection.query(sqlForSelectBoard, check_data, function(err, data){
+          
+          // New Member -> Insert
+          if(data == ""){
+              sqlForInsertBoard = "insert into nobell.owner(owner_name, owner_email, owner_pwd, owner_phone) values(?,?,?,?)";
+              connection.query(sqlForInsertBoard, reg_data, function(err, rows){
+                  if(err) {
+                      console.log('-> Fail to Insert : ', err);
+                      res.send("fail:505");
+                  }
+                  else {
+                      console.log('-> Success to Register ! ');
+                      res.send("success");
+                  }
+              });
+          }
+          
+          // Exist Member : Fail
+          else{
+              console.log('-> Fail to Register : Exist Member');
+              res.send("fail:500");
+          }
+
+      });
+  });
+});
+
 
 
 
