2473a2954fa4e40a610e6b6860296797678f89e2
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-08-29 03:22:09 +0900

    Update Owner Router : Insert, Update, Delete 'Menu'

--- WebServer/routes/owner.js
+++ WebServer/routes/owner.js
@@ -243,6 +243,55 @@ router.post('/edit_owner', function(req, res, next){
   });
 });
 
+// Get ALL Menus of Restaurant Request
+router.get('/menu/:id', function(req, res) {
+    console.log('[GET All Menus Request]');
+    console.log('-> Get rs_id = ', req.params.id);
+
+    pool.getConnection(function(err, connection){
+        var sqlForMenu = "select * FROM nobell.menu WHERE menu_rs_id=?";
+
+        connection.query(sqlForMenu, req.params.id, function(err, rows){
+            if(err) res.send("fail:500");
+            else res.send(rows);
+        });
+    });
+});
+
+// Edit Menu of Restaurant Request
+router.post('/menu', function(req, res, next){
+    console.log('[POST Edit Menu Request]');
+    console.log('-> Get rs_id = ', req.body.rs_id);
+    console.log('-> Get operation = ', req.body.operation);
+
+    var rs_id = req.body.rs_id;
+    var menu_name = req.body.menu_name;
+    var menu_price = req.body.menu_price;
+
+    var operation = req.body.operation;
+
+    // Classify by operation
+    if(operation == "new"){
+        menu_data = [rs_id, menu_name, menu_price];
+        var sqlForMenu = "INSERT INTO nobell.menu values(?,?,?)";
+    }
+    else if(operation == "update"){
+        menu_data = [menu_price, rs_id, menu_name];
+        var sqlForMenu = "UPDATE nobell.menu SET menu_price=? WHERE menu_rs_id=? AND menu_name=?";
+    }
+    else if(operation == "delete"){
+        menu_data = [rs_id, menu_name];
+        var sqlForMenu = "DELETE FROM nobell.menu WHERE menu_rs_id=? AND menu_name=?";
+    }
+    
+    // Connect DB
+    pool.getConnection(function(err, connection){
+        connection.query(sqlForMenu, menu_data, function(err, rows){
+            if(err) res.send("fail:500");
+            else res.send('success');
+        });
+    });
+});
 
 
 module.exports = router;
