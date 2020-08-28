b54cc6ad609ca7cc4806edc522ed9d0e1749d910
YuHyeonGeun-KOR <63186208+YuHyeonGeun-KOR@users.noreply.github.com>
2020-08-29 01:30:20 +0900

    Proceeding : MySQL <=> php <=> android

--- AndroidApp/User/app/src/main/java/com/nobell/user/MainActivity.java
+++ AndroidApp/User/app/src/main/java/com/nobell/user/MainActivity.java
@@ -2,13 +2,136 @@ package com.nobell.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et_id;
+    private EditText et_pwd;
+    private Button btn_login, btn_register;
+    private String lo_id , lo_pw;
+    private String result_login;
+    String data_l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        et_id = findViewById(R.id.et_id);
+        et_pwd = findViewById(R.id.et_pwd);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lo_id = et_id.getText().toString();
+                lo_pw = et_pwd.getText().toString();
+
+                // Making AsyncTask For Server
+                LoginTask logintask = new LoginTask();
+
+                // Execute LoginTask
+                try {
+                    data_l = logintask.execute().get().trim();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (data_l.equals("LogOK")) {
+                    Toast.makeText(MainActivity.this, "로그인에 성공하였습니다", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, ItemList.class);
+                    startActivity(intent);
+                }
+                // Fail
+                else {
+                    if (data_l.equals("Incorrect")) {
+                        Toast.makeText(MainActivity.this, "가입된 정보와 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "오류가 발생하였습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+    }
+
+
+
+    public class LoginTask extends AsyncTask<Void, Integer, String> {
+
+        @Override
+        protected String doInBackground(Void... unused) {
+
+
+            String param = "&login_email=" + lo_id+ "&login_pwd=" + lo_pw  + "";
+            try {
+
+                URL url = new URL("http://172.20.10.2:3000/user/login");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.connect();
+
+                /* 안드로이드 -> 서버 파라메터값 전달 */
+                OutputStream outs = conn.getOutputStream();
+                outs.write(param.getBytes("UTF-8"));
+                outs.flush();
+                outs.close();
+
+                /* 서버 -> 안드로이드 파라메터값 전달 */
+                InputStream is = null;
+                BufferedReader in = null;
+
+
+                is = conn.getInputStream();
+                in = new BufferedReader(new InputStreamReader(is), 8 * 1024);
+                String line = null;
+                StringBuffer buff = new StringBuffer();
+                while ( ( line = in.readLine() ) != null )
+                {
+                    buff.append(line + "\n");
+                }
+                result_login = buff.toString();
+                data_l = result_login.trim();
+                Log.e("RECV DATA", data_l);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return data_l;
+        }
+
     }
 }
--- WebServer/routes/user.js
+++ WebServer/routes/user.js
@@ -1,9 +1,93 @@
 var express = require('express');
+var mysql = require('mysql');
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  database: 'nobell',
+  password : 'asdasd',
+ 
+});
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+    res.send(200);
 });
+router.post('/hi', function(req, res) {
+    res.send("LogOK");
+});
+
+router.post('/test', function(req, res) {
+    res.send("LogOK");
+});
+// Register
+router.post('/register', function(req, res, next) {
+
+  var user_name = req.body.reg_name;
+  var user_email = req.body.reg_email;
+  var user_pwd = req.body.reg_pwd;
+  var user_phone = req.body.reg_phone;
+  var message = 'Exist';
+
+  var check_data = null;
+ 
+  var reg_data = [user_email, user_name,user_pwd, user_phone];
+
+  console.log('-> Get Data = ', reg_data);
+  pool.getConnection(function(err, connection){
+      
+    connection.query("INSERT INTO nobell.user_info(user_email, user_name, user_pwd, user_phone) values(?,?,?,?)", reg_data, function(err,data){
+        if(err) {
+            if(err.errno == 1062)
+                res.send("Exist");
+            else{
+                res.send('Resiter error')
+            }
+        }
+        else {
+            res.send("RegOK");
+        }
+    });
+  });
+});
+
+
+// Login
+router.post('/login', function(req, res, next) {
+
+  console.log('-> Get Email = ', req.body.login_email, '-> Get pwd = ' ,req.body.login_pwd);
+
+  var login_email = req.body.login_email;
+  var login_pwd = req.body.login_pwd;
+
+  var login_data = [login_email, login_pwd];
+  
+  pool.getConnection(function(err, connection){
+
+      connection.query("select * FROM nobell.user_info WHERE user_email=? AND user_pwd=?", login_data, function(err, data){
+
+          if(err) {
+              res.send('Error');
+          }
+          else {
+              if(data == "") {
+                  res.send('Incorrect');
+              }
+
+              else {
+                  res.send("LogOK");
+              }
+          }
+      });
+  });
+});
+
+
+
+
 
 module.exports = router;
